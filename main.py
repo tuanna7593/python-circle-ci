58d4a9b9d326f630243c8fe21ce49a6887efc607
Dao Nguyen <nguyenthitrucdao@Nguyens-MacBook-Air.local>
2019-07-18 21:29:35 +0700

    Add new test

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ def func(x):
 
 def test_answer():
     assert func(3) == 5
+
+def sum(x, y):
+    return x + y + 1
+
+def simple_unittest_sum():
+    assert sum(3, 3) == 6, "should be 6"
+
+if __name__ == "__main__":
+    simple_unittest_sum()
+    print("Everything is passed")
